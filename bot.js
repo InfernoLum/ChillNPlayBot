966200256ae2318f03238ae2ca01233926e86e91
InfernoLum <42121738+InfernoLum@users.noreply.github.com>
2018-09-30 14:27:47 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -45,10 +45,10 @@ client.on('message', message => {
 
 	let command = message.content.slice(prfx.length).trim().split(/ +/g).shift().toLowerCase();
 	let args = message.content.slice(prfx.length).slice(command.length).toLowerCase();
-	
+
 	if(message.content.startsWith(prfx + command))
 	{
-
+					
 		switch(command)
 		{
 			case "ping": message.channel.send("Pong!"); break;
@@ -68,104 +68,26 @@ client.on('message', message => {
 			}
 			break;
 			case "flip":
-			
-				let _coin = Math.floor((Math.random() * 2));
-				switch(_coin)
-				{
-					case 0: message.channel.send("You win!");const Discord = require('discord.js');
-					const client = new Discord.Client();
-					const token = process.env.token
-					const fs = require('fs');
-					
-					let prfx = '?';
-					
-					
-					let userData = JSON.parse(fs.readFileSync('userData.json' , 'utf8'));
-					
-						
-						//Vars
-						let sender = message.author;
-						let msg = message.content.toUpperCase();
-					
-					
-						//Events
-						if (!userData[sender.id]) userData[sender.id] = {}
-						if(!userData[sender.id].money) userData[sender.id].money = 600;
-					
-						if (message.content === 'ping') {
-							message.reply('pong');
-						}
-						  
-						if (message.content === 'flip') {
-							message.reply('flop');
-						}
-						
-						if(message.content === 'money')
-						{
-							message.reply(userData[sender.id].money);
-						}
-					
-						if(message.content === 'getmymoney')
-						{
-							userData[sender.id].money -= 100;
-							message.reply("I took your money" + userData[sender.id].money);
-						}
-					
-						let command = message.content.slice(prfx.length).trim().split(/ +/g).shift().toLowerCase();
-						let args = message.content.slice(prfx.length).slice(command.length).toLowerCase();
-						
-						if(message.content.startsWith(prfx + command))
-						{
-					
-							switch(command)
-							{
-								case "ping": message.channel.send("Pong!"); break;
-								case "8ball":
-								if(message.content.length >= 6)
-								{
-									console.log("Something should happen now");
-									let _answer = Math.floor((Math.random() * 5));
-									switch(_answer)
-									{
-										case 0: message.channel.send("Most likely"); break;
-										case 1: message.channel.send("Yes"); break;
-										case 2: message.channel.send("Maybe"); break;
-										case 3: message.channel.send("Not likely"); break;
-										case 4: message.channel.send("No"); break;
-									}
-								}
-								break;
-								case "flip":
 								
-									let _coin = Math.floor((Math.random() * 2));
-									let FlippedCoins = parseInt(args);
-									switch(_coin)
-									{
-										case 0: message.channel.send("You win!"); 
-											userData[sender.id].money += 300; 
-										break;
+				let _coin = Math.floor((Math.random() * 2));
+				let FlippedCoins = parseInt(args);
+			switch(_coin)
+			{
+				case 0: message.channel.send("You win!"); 
+					userData[sender.id].money += 300; 
+				break;
 
-										case 1: message.channel.send("You lose!"); 
-											userData[sender.id].money -= FlippedCoins; 
-										break;
-									}
-									message.channel.send(args);
-								break;
-								case "cmds": 
-									message.channel.send(":regional_indicator_c: :regional_indicator_m: :regional_indicator_d: :regional_indicator_s:");
-									message.channel.send(":one: *** - PING***");
-									message.channel.send(":two: *** - 8BALL***");
-									message.channel.send(":three: *** - FLIP***"); break;
-							}
-						}
-					
-				}
-			break;
-			case "cmds": 
-				message.channel.send(":regional_indicator_c: :regional_indicator_m: :regional_indicator_d: :regional_indicator_s:");
-				message.channel.send(":one: *** - PING***");
-				message.channel.send(":two: *** - 8BALL***");
-				message.channel.send(":three: *** - FLIP***"); break;
+				case 1: message.channel.send("You lose!"); 
+					userData[sender.id].money -= FlippedCoins; 
+				break;
+			}
+				message.channel.send(args);
+
+				case "cmds": 
+					message.channel.send(":regional_indicator_c: :regional_indicator_m: :regional_indicator_d: :regional_indicator_s:");
+					message.channel.send(":one: *** - PING***");
+					message.channel.send(":two: *** - 8BALL***");
+					message.channel.send(":three: *** - FLIP***"); break;
 		}
 	}
 });
