84a7dac5b0c55b61cf1474e6b6695fda2ae600bc
InfernoLum <42121738+InfernoLum@users.noreply.github.com>
2018-10-03 21:24:51 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,6 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const token = process.env.token
 const fs = require('fs');
-const ms = require('parse-ms');
 
 let prfx = '?';
 
